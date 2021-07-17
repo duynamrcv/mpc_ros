39b152e4a5d73fa5356527c69486afe5c382648d
duynamrcv <duynam.robotics@gmail.com>
2021-07-17 10:37:06 +0700

    Update path generator

--- src/make_path.cpp
+++ src/make_path.cpp
@@ -9,9 +9,9 @@
 
 using namespace std;
 
-bool path_generator(const double& radius, nav_msgs::Path& path);
-string frame_id;
-double radius;
+bool path_generator(nav_msgs::Path& path);
+string frame_id, x_func, y_func;
+double x_radius, y_radius, kx, ky;
 
 int main(int argc, char** argv)
 {
@@ -20,15 +20,21 @@ int main(int argc, char** argv)
 
     ros::Publisher path_pub = nh.advertise<nav_msgs::Path>("/path", 1);
 
+    /* ROS PARAMS */
     ros::param::get("~frame_id", frame_id);
-    ros::param::get("~radius", radius);
+    ros::param::get("~x_radius", x_radius);
+    ros::param::get("~y_radius", y_radius);
+    ros::param::get("~kx", kx);
+    ros::param::get("~ky", ky);
+    ros::param::get("~x_func", x_func);
+    ros::param::get("~y_func", y_func);
 
-    cout << frame_id << " " << radius << endl;
+    cout << frame_id << " " << x_radius << " " << y_radius << endl;
 
     nav_msgs::Path path;
     
 
-    while(!path_generator(radius, path)) {}
+    while(!path_generator(path)) {}
     ros::Rate r(10);
     while(ros::ok())
     {
@@ -38,7 +44,7 @@ int main(int argc, char** argv)
     }
 }
 
-bool path_generator(const double& radius, nav_msgs::Path& path)
+bool path_generator(nav_msgs::Path& path)
 {
     // Generate the circle path
     double Ts = 0.1;
@@ -50,8 +56,24 @@ bool path_generator(const double& radius, nav_msgs::Path& path)
     path.header.stamp = ros::Time::now();
     for (int i = 0; i < 400; i++)
     {
-        double x = radius*sin(PI/4 * i * Ts * 0.2);
-        double y = radius*cos(PI/4 * i * Ts * 0.2);
+        double x, y;
+        if (x_func == "cos")
+        {
+            x = x_radius*cos(PI/4 * i * Ts * kx * 0.2);
+        }
+        else
+        {
+            x = x_radius*sin(PI/4 * i * Ts * kx * 0.2);
+        }
+        
+        if (y_func == "cos")
+        {
+            y = y_radius*cos(PI/4 * i * Ts * ky * 0.2);
+        }
+        else
+        {
+            y = y_radius*sin(PI/4 * i * Ts * ky * 0.2);
+        }
 
         geometry_msgs::PoseStamped pose;
         pose.header = path.header;
